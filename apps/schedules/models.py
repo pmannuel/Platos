a63d835b1b2f6846a12c240aeae198df64317206
Nishant Aggarwal <nish8192@gmail.com>
2017-02-27 14:06:51 -0500

    Added Schedule model and Schedule Manager model

--- apps/schedules/models.py
+++ apps/schedules/models.py
@@ -1,8 +1,64 @@
 from __future__ import unicode_literals
-
+from ..login_register.models import User
 from django.db import models
 
 # Create your models here.
+class ScheduleManager(models.Manager):
+    def isMatch(self, request):
+        mon = []
+        tue = []
+        wed = []
+        thu = []
+        fri = []
+        sat = []
+        sun = []
+        i = 0
+        while(i<=23):
+            if Schedule.objects.filter(mon_schedule__(i)_(i+1)).filter(user = session['user_id']) == True:
+                mon.append(Schedule.objects.filter(mon_schedule__(i)_(i+1).exclude(user = request.session['user_id'])))
+            i += 1
+        i = 0
+        while(i<=23):
+            if Schedule.objects.filter(tue_schedule__(i)_(i+1)).filter(user = request.session['user_id']) == True:
+                tue.append(Schedule.objects.filter(tue_schedule__(i)_(i+1).exclude(user = request.session['user_id'])))
+            i += 1
+        i = 0
+        while(i<=23):
+            if Schedule.objects.filter(wed_schedule__(i)_(i+1)).filter(user = request.session['user_id']) == True:
+                wed.append(Schedule.objects.filter(wed_schedule__(i)_(i+1).exclude(user = request.session['user_id'])))
+            i += 1
+        i = 0
+        while(i<=23):
+            if Schedule.objects.filter(thu_schedule__(i)_(i+1)).filter(user = request.session['user_id']) == True:
+                thu.append(Schedule.objects.filter(thu_schedule__(i)_(i+1).exclude(user = request.session['user_id'])))
+            i += 1
+        i = 0
+        while(i<=23):
+            if Schedule.objects.filter(fri_schedule__(i)_(i+1)).filter(user = request.session['user_id']) == True:
+                fri.append(Schedule.objects.filter(fri_schedule__(i)_(i+1).exclude(user = request.session['user_id'])))
+            i += 1
+        i = 0
+        while(i<=23):
+            if Schedule.objects.filter(sat_schedule__(i)_(i+1)).filter(user = request.session['user_id']) == True:
+                sat.append(Schedule.objects.filter(sat_schedule__(i)_(i+1).exclude(user = request.session['user_id'])))
+            i += 1
+        i = 0
+        while(i<=23):
+            if Schedule.objects.filter(sun_schedule__(i)_(i+1)).filter(user = request.session['user_id']) == True:
+                sun.append(Schedule.objects.filter(sun_schedule__(i)_(i+1).exclude(user = request.session['user_id'])))
+            i += 1
+        matches = {
+        'mon': mon,
+        'tue': tue,
+        'wed': wed,
+        'thu': thu,
+        'fri': fri,
+        'sat': sat,
+        'sun':sun
+        }
+        return matches
+
+
 class Schedule(models.Model):
     user = models.ForeignKey(User, related_name = 'schedule_user')
     mon = models.ForeignKey(Day, related_name = 'mon_schedule')
@@ -12,13 +68,10 @@ class Schedule(models.Model):
     fri = models.ForeignKey(Day, related_name = 'fri_schedule')
     sat = models.ForeignKey(Day, related_name = 'sat_schedule')
     sun = models.ForeignKey(Day, related_name = 'sun_schedule')
-    mon2 = models.ForeignKey(Day, related_name = 'mon_schedule')
-    tue2 = models.ForeignKey(Day, related_name = 'tue_schedule')
-    wed2 = models.ForeignKey(Day, related_name = 'wed_schedule')
-    thu2 = models.ForeignKey(Day, related_name = 'thu_schedule')
-    fri2 = models.ForeignKey(Day, related_name = 'fri_schedule')
-    sat2 = models.ForeignKey(Day, related_name = 'sat_schedule')
-    sun2 = models.ForeignKey(Day, related_name = 'sun_schedule')
+    objects = ScheduleManager()
+
+
+
 class Day(models.Model):
     9_10 = models.BooleanField(False)
     10_11 = models.BooleanField(False)

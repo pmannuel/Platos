616aa7d097758ef5dfb6a3b206d614d5a4ae6ee2
hermanj13 <hermanj13@me.com>
2017-03-01 11:24:16 -0500

    absolute path

--- Platos/settings.py
+++ Platos/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -122,5 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = '/Users/jakeherman/Documents/Coding/Platos/apps/login_register/media'
+MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
 MEDIA_URL = '/media/'

a560ed8a52c70cf66cfbd0b1deea31c83eb859ab
Nishant Aggarwal <nish8192@gmail.com>
2017-03-02 17:00:59 -0500

    Minor edit_profile changes

--- apps/main/views.py
+++ apps/main/views.py
@@ -21,26 +21,21 @@ def index(request):
             lat2 = other_user.latitude
             lon2 = other_user.longtitude
 
+            print other_user.id
+
             dist = haversine((lat1, lon1), (lat2, lon2), miles=True)
 
-            if not Match.objects.filter(this_user_id=user_id).filter(profile_id=other_user.id).exists():
+            if not Match.objects.filter(this_user_id=user_id, profile_id=other_user.id).exists():
                 Match.objects.create(
                     this_user = User.objects.get(id=user_id),
                     profile = Profile.objects.get(id=other_user.id),
                     distance = dist
                 )
-            else:
-                Match.objects.filter(this_user_id=user.id).update(
-                    this_user = User.objects.get(id=user_id),
-                    profile = Profile.objects.get(id=other_user.id),
-                    distance = dist
-                )
 
-        setDistance = Profile.objects.only('distAway').get(user_id = request.session['active_user_id']).distAway
         data = {
             "user" : User.objects.get(id=user_id),
             "profile" : user,
-            "other_users" : Match.objects.filter(this_user_id=user_id).filter(distance__gte=setDistance),
+            "other_users" : Match.objects.filter(this_user_id=user_id),
             'flag' : True
             }
 

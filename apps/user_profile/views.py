f7f87acdd5d1d58820d4e362341c5aca03a82ef4
pmannuel <pmannuel@gmail.com>
2017-03-03 13:25:14 -0500

    Update

--- apps/user_profile/views.py
+++ apps/user_profile/views.py
@@ -1149,7 +1149,7 @@ def update_times(request):
         schedule.sun = sun
         schedule.save()
 
-    return redirect(reverse('user_profile:edit_profile', kwargs={'user_id': request.session['active_user_id']}))
+    return redirect(reverse('user_profile:index', kwargs={'user_id': request.session['active_user_id']}))
 
 def edit_profile(request, user_id):
     user_id = request.session.get('active_user_id')
